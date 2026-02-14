3ec25fbaa00a651bec2e443d1b084626a3147c2a
101zh <67253838+101zh@users.noreply.github.com>
2026-02-13 17:38:54 -0800

    offset labs when hovering over a lab

--- networking/script.js
+++ networking/script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container")
+const labs = []
 
 const html =
   `         <div class="block">
@@ -13,7 +14,7 @@ fetch("labs.json")
     for (let i = 0; i < labInfoArray.length; i++) {
       const lab = labInfoArray[i];
       console.log(container)
-      container.appendChild(createElementFromHTML(html.replaceAll("filename", lab["filename"]).replace("labname", lab["name"]), "./labs/" + lab["filename"] + ".pdf"))
+      container?.appendChild(createElementFromHTML(html.replaceAll("filename", lab["filename"]).replace("labname", lab["name"]), "./labs/" + lab["filename"] + ".pdf"))
     }
   })
   .catch((e) => console.error(e));
@@ -24,5 +25,45 @@ function createElementFromHTML(htmlString, href) {
   a.innerHTML = htmlString.trim();
   a.href = href
 
+  a.onmouseover = function () { interactivelyMove(a) }
+  a.onmouseout = function () { resetLabPositions() }
+  labs.push(a)
+
   return a;
+}
+
+function interactivelyMove(hoveredLab) {
+  hoveredLab = hoveredLab.firstChild
+
+  labs.forEach(element => {
+    element = element.firstChild
+
+    var moveDir = [element.getBoundingClientRect().x - hoveredLab.getBoundingClientRect().x, element.getBoundingClientRect().y - hoveredLab.getBoundingClientRect().y]
+
+    moveDir[0] /= 14
+    moveDir[1] /= 10
+
+    var magnitude = Math.sqrt(moveDir[0] * moveDir[0] + moveDir[1] * moveDir[1])
+    if (magnitude != 0) {
+      console.log(magnitude)
+
+      moveDir[0] = moveDir[0] * 0.45  
+      moveDir[1] = moveDir[1] * 0.45
+
+      element.style.transition = "all 0.25s"
+      element.style.left = moveDir[0].toString() + "px";
+      element.style.top = moveDir[1].toString() + "px";
+      console.log(moveDir)
+    }
+
+  });
+}
+
+function resetLabPositions() {
+  labs.forEach(element => {
+    element = element.firstChild
+
+    element.style.left = "0rem";
+    element.style.top = "0rem";
+  });
 }
