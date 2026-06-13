9245816af94cbfca8130aae6643877d2886ee212
Hunter Zhuang <67253838+101zh@users.noreply.github.com>
2026-06-13 15:06:08 -0700

    1.1.1 (#5)
    * rename lab container id for clarity
    
    * make cursor a pointer on clickable items
    
    * add networking certifications
    
    * use .webp for lab thumbnails
    
    * change title

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-const container = document.getElementById("networkingSectionBody")
-const labs = []
+const labContainer = document.getElementById("networkingSectionBody")
+const certContainer = document.getElementById("cert-section-body")
 var labInfoArray = "";
 
 const maxLabsShown = 4
 
-const html =
-    `   <div class="block on-hover-up">
+const labBlock =
+    `   <div class="block lab-block on-hover-up">
             <div class="blockTitle">labname</div>
             <div class="blockBody">ONELINER</div>
         </div>
     `
+const certBlock =
+    `<div class="block">
+        <div class="blockTitle cert-title">
+            <div class="cert-logo"><img src="../assets/cert-thumbnails/filename.png">
+            </div> certname
+        </div>
+        <div class="blockBody cert-body">
+            <a href="VERIFICATION" target="_blank" rel="noopener noreferrer" class="cert-button" id="repo-link">
+                <div class="on-hover-up animated-underline">
+                    <div class="cert-button-symbol"><img src="../assets/symbols/checkmark-symbol.svg">
+                    </div>
+                    <div class="cert-button-text">Verify</div>
+                </div>
+            </a>
+            <a href="INFO" target="_blank" rel="noopener noreferrer" class="cert-button" id="repo-link">
+                <div class="on-hover-up animated-underline">
+                    <div class="cert-button-symbol"><img src="../assets/symbols/info-icon.svg">
+                    </div>
+                    <div class="cert-button-text">Info</div>
+                </div>
+            </a>
+        </div>
+    </div>
+    `
+
+fetch("./assets/certs.json")
+    .then(response => response.text())
+    .then((textContent) => {
+        certArray = JSON.parse(textContent)["certs"]
+        for (let i = 0; i < certArray.length; i++) {
+            const cert = certArray[i];
+
+            certContainer?.appendChild(createCertFromHTML(certBlock.replace("filename", cert["filename"]).replace("certname", cert["name"]).replace("VERIFICATION", cert["verification"]).replace("INFO", cert["info"])))
+        }
+    })
+    .catch((e) => console.error(e));
+
+function createCertFromHTML(htmlString) {
+    var div = document.createElement('div')
+    div.innerHTML = htmlString.trim()
+    div.style.display = "flex"
+
+    return div;
+}
 
 fetch("./assets/labs.json")
     .then(response => response.text())
@@ -21,15 +65,14 @@ fetch("./assets/labs.json")
             labInfoArray[i] = lab;
 
             if (i < maxLabsShown) {
-                container?.appendChild(createElementFromHTML(html.replace("filename", lab["filename"]).replace("labname", lab["name"]).replace("ONELINER", lab["one-liner"]), lab))
+                labContainer?.appendChild(createLabFromHTML(labBlock.replace("filename", lab["filename"]).replace("labname", lab["name"]).replace("ONELINER", lab["one-liner"]), lab))
             }
         }
     })
     .catch((e) => console.error(e));
 
 
-function createElementFromHTML(htmlString, lab) {
-    console.log(lab)
+function createLabFromHTML(htmlString, lab) {
     var div = document.createElement('div')
     div.innerHTML = htmlString.trim()
     div.style.display = "flex"
